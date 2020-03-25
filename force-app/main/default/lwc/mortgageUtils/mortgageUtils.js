5cacbab78ac1b3fae88f2ddfcc0796f439862f88
mrazdan88 <37470102+mrazdan88@users.noreply.github.com>
2020-03-25 23:27:02 +0530

    Update mortgageUtils.js

--- force-app/main/default/lwc/mortgageUtils/mortgageUtils.js
+++ force-app/main/default/lwc/mortgageUtils/mortgageUtils.js
@@ -1,10 +1,11 @@
 /* structure for utility class */
 /*Definition of classes */
-const calculateMonthlyPayment = (principal, years, rate) => {
-    if (principal && years && rate && rate >0) {
+/* Modified the code to assume that months are coming in the years variable so replacing year with months*/
+const calculateMonthlyPayment = (principal, months, rate) => {
+    if (principal && months && rate && rate >0) {
         const monthlyRate = rate/100/12;
         const monthlyPayment = 
-            (principal * monthlyRate) / (1 - Math.pow(1/ (1 + monthlyRate), years * 12));
+            (principal * monthlyRate) / (1 - Math.pow(1/ (1 + monthlyRate), months));
             return monthlyPayment;
     }
     return 0;
